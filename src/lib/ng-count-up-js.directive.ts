cb629a8f3a1a8f84277b3a471eb32c056b5c7fd7
YoungHyeong Ryu <wangta69@naver.com>
2019-06-13 14:25:26 +0900

    Summary

--- src/lib/ng-count-up-js.directive.ts
+++ src/lib/ng-count-up-js.directive.ts
@@ -1,12 +1,12 @@
 import {
-  Directive,
-  ElementRef,
-  Input,
-  Output,
-  HostListener,
-  EventEmitter,
-  OnChanges,
-  SimpleChanges
+    Directive,
+    ElementRef,
+    Input,
+    Output,
+    HostListener,
+    EventEmitter,
+    OnChanges,
+    SimpleChanges
 } from '@angular/core';
 import { CountUp } from './countUp';
 
@@ -15,86 +15,80 @@ import { CountUp } from './countUp';
 })
 export class CountUpDirective implements OnChanges {
 
-  countUp: any;
-  options: any;
-  // Optional extra configuration, such as easing.
-  // @Input('countUp') options: any;
+    countUp: any;
+    options: any;
+    // Optional extra configuration, such as easing.
+    // @Input('countUp') options: any;
 
-  // Optional start value for the count. Defaults to zero.
-  @Input() startVal: number;
+    // Optional start value for the count. Defaults to zero.
+    @Input() startVal: number;
 
-  // the number to count to
-  @Input() endVal: number;
+    // the number to count to
+    @Input() endVal: number;
 
-  // Optional duration of the animation in seconds. Default is 2.
-  @Input() duration: number;
+    // Optional duration of the animation in seconds. Default is 2.
+    @Input() duration: number;
 
-  // Optional number of decimal places. Default is 2.
-  @Input() decimals: number;
+    // Optional number of decimal places. Default is 2.
+    @Input() decimals: number;
 
-  // Optional flag for specifying whether the element should re-animate when clicked.
-  // Default is true.
-  @Input() reanimateOnClick: boolean;
+    // Optional flag for specifying whether the element should re-animate when clicked.
+    // Default is true.
+    @Input() reanimateOnClick: boolean;
 
-  // on-complete event emitter
-  @Output() complete = new EventEmitter<void>();
+    // on-complete event emitter
+    @Output() complete = new EventEmitter<void>();
 
-  // Re-animate if preference is set.
-  @HostListener('click')
-  onClick() {
-    if (this.reanimateOnClick) {
-      this.animate();
-    }
-  }
-
-  constructor(private el: ElementRef) {}
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.endVal && typeof changes.endVal.currentValue !== 'undefined') {
-      this.countUp = this.createCountUp();
-      this.animate();
-    }
-  }
-
-  private createCountUp(): any {
-    const start = this.startVal || 0;
-    const duration = this.duration || 2;
-    const decimals = this.decimals || 0;
-
-    if (!this.duration) {
-      this.duration = duration;
+    // Re-animate if preference is set.
+    @HostListener('click')
+    onClick() {
+        if (this.reanimateOnClick) {
+            this.animate();
+        }
     }
 
-    this.options = {
-        start: start,
-        duration: duration,
-        decimals: decimals
-    };
+    constructor(private el: ElementRef) {}
 
-    // construct countUp
-
-    let countUp = new CountUp(this.el.nativeElement, this.endVal, this.options);
-    const diff = Math.abs(this.endVal - start);
-    // make easing smoother for large numbers
-    if (diff > 999) {
-      const up = (this.endVal > start) ? -1 : 1;
-      countUp = new CountUp(this.el.nativeElement, this.endVal + (up * 100), this.options);
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes.endVal && typeof changes.endVal.currentValue !== 'undefined') {
+            this.countUp = this.createCountUp();
+            this.animate();
+        }
     }
 
-    return countUp;
-  }
+    private createCountUp(): any {
+        const start = this.startVal || 0;
+        const duration = this.duration || 2;
+        const decimals = this.decimals || 0;
 
-  private animate() {
-    this.countUp.reset();
-    if (this.endVal > 999) {
-      this.countUp.start(() => this.countUp.update(this.endVal));
-      setTimeout(() => {
-        this.complete.emit();
-      }, this.duration * 1000);
-    } else {
-      this.countUp.start(() => {
-        this.complete.emit();
-      });
+        if (!this.duration) {
+            this.duration = duration;
+        }
+
+        this.options = {
+            start: start,
+            duration: duration,
+            decimals: decimals
+        };
+
+        // construct countUp
+
+        let countUp = new CountUp(this.el.nativeElement, this.endVal, this.options);
+        const diff = Math.abs(this.endVal - start);
+        // make easing smoother for large numbers
+        if (diff > 999) {
+            const up = (this.endVal > start) ? -1 : 1;
+            countUp = new CountUp(this.el.nativeElement, this.endVal + (up * 100), this.options);
+        }
+
+        return countUp;
     }
-  }
+
+    private animate() {
+        this.countUp.reset();
+            this.countUp.start(() => this.countUp.update(this.endVal));
+            setTimeout(() => {
+                this.complete.emit();
+            }, this.duration * 1000);
+    }
 }
